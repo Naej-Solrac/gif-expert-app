8e5cea726b6a38189c2dcb0670a2efb3c3f4f6af
Jean Gonzales <expretapador3@gmail.com>
2023-04-10 19:34:19 -0500

    agregar una nueva categoria ...categories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
-
+import { useState } from 'react'
 export const GifExpertApp = () => {
+
+  //hook para mantener el estado 
+  const [ categories, setCategories ] = useState(['One punch', 'Dragon ball'])
+  console.log(categories);
+
+  const onAddCategory = () => {
+
+    setCategories( [...categories, 'jean'] ) // una forma de agregar categoria
+    //setCategories( cat =>  [...categories, 'jean']) // otra forma de hacelo
+  }
+
   return (
     <>
       { /*titulo*/ }
@@ -9,6 +20,13 @@ export const GifExpertApp = () => {
       { /*Input*/ }
 
       { /*Listado de Gif*/ }
+      <button onClick={onAddCategory}>agregar</button>
+      <ol>
+        {categories.map(category => {
+          return <li key={category}> { category } </li>
+          })
+        }
+      </ol>
         { /*Gif Item*/ }
     </>
   )
