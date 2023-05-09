2b22f2098780c3976a21b85c81a9ac97dbbfa45e
Jean Gonzales <expretapador3@gmail.com>
2023-05-09 17:32:10 -0500

    creando custom hook, y creando isLoading

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,21 +1,11 @@
-import { useEffect } from "react";
-import { getGifs } from "./helpers/getGifs";
-import { useState } from "react";
 import { GifItem } from "./GifItem";
+import { useFetchGifs } from "./hooks/useFetchGifs";
 
 export const GifGrid = ( {category} ) => {
 
-  const [images, setImages] = useState( [] )
+  const { images , isLoading} = useFetchGifs( category );
+  console.log({images, isLoading});
 
-  const getImages = async() => {
-    const newImages = await getGifs( category );
-    setImages(newImages);
-  }
-
-  useEffect( () => {
-    getImages();
-  }, [])
-  
 
   return (
     <>
